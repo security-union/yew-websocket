58fe9979c7ef322c514caa4bcb449201d3b98b0f
Dario Lencina <dario@securityunion.dev>
2022-09-11 00:13:43 -0400

    save

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,34 @@
 //! Contains three macros for wrapping serde format.  Collectively they
 //! allow you to define your own text and binary wrappers.
 
+/**
+ * Copyright (c) 2017 Denis Kolodin
+
+Permission is hereby granted, free of charge, to any
+person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the
+Software without restriction, including without
+limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice
+shall be included in all copies or substantial portions
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+ */
+
 /// This macro is used for a format that can be encoded as Text.  It
 /// is used in conjunction with a type definition for a tuple struct
 /// with one (publically accessible) element of a generic type.  Since
