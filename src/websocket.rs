e2cfc5822ef41de1000f7428d7ae1629fbeebe27
Dario Lencina <dario@securityunion.dev>
2023-04-09 08:00:48 -0400

    fmt

--- src/websocket.rs
+++ src/websocket.rs
@@ -28,7 +28,6 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
  */
-
 use anyhow::Error;
 use std::fmt;
 use thiserror::Error as ThisError;
